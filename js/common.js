20d6140699bab563e8c237a2082aa7fe95ef52f6
Fedor Kudinov <fedor.kudinov.dvlopbiz@gmail.com>
2015-12-03 11:09:36 +0200

    remove min.css and min.js file add CDN js links add pixlayout.js add package.json

--- js/common.js
+++ js/common.js
@@ -1,34 +1,31 @@
 'use strict';
+// jQuery function to execute when the DOM is fully loaded.
+/*
+  jQuery(document).ready(function () {});
+  || or
+  $(document).ready(function () {});
+  || or
+  $(function () {});
+*/
+
 $(document).ready(function () {
 
-    $("html").removeClass("no-js").addClass("js");
+  $("html").removeClass("no-js").addClass("js");
 
-    $('.accordion-switch').on('click', function () {
+  $('.accordion-switch').on('click', function () {
 
-        var accordion = $('.accordion');
-        var this_accordion = $(this).closest(accordion);
+    var accordion = $('.accordion'),
+      this_accordion = $(this).closest(accordion);
 
-        if (this_accordion.hasClass('js-opened')) {
-            this_accordion.removeClass('js-opened');
-        } else {
-            accordion.removeClass('js-opened');
-            this_accordion.addClass('js-opened');
-        }
+    if (this_accordion.hasClass('js-opened')) {
+      this_accordion.removeClass('js-opened');
+    } else {
+      accordion.removeClass('js-opened');
+      this_accordion.addClass('js-opened');
+    }
 
-    });
+  });
 
 });
 
-;(function () {
-
-})();
-
-/*
-jQuery(document).ready(function () {});
-
-$(document).ready(function () {});
-
-$(function () {});
-
-;(function () {})();
-*/
+(function () {})();
