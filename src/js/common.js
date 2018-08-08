4d33ce6cb974f7943b9397e2cd69f3dc4bae9981
Kudinov Fedor <brothersrabbits@mail.ru>
2018-08-08 10:38:01 +0300

    Add js function movingLine; Update dependencies and js libs;

--- src/js/common.js
+++ src/js/common.js
@@ -432,4 +432,54 @@
         });
     }
 
+    /**
+     * Moving Line
+     *
+     * @example
+     * movingLine();
+     * @author Fedor Kudinov <brothersrabbits@mail.ru>
+     */
+    function movingLine() {
+        var nav = document.querySelector('.nav');
+
+        /**
+         * Line CSS style
+         *
+         * @example
+         * lineStyle(line, element);
+         * @author Fedor Kudinov <brothersrabbits@mail.ru>
+         * @param {Object} line - The line for which styles will be given
+         * @param {Object} element - The element from which the style values for the line are taken
+         */
+        function lineStyle(line, element) {
+            if (element) {
+                line.style.width = element.clientWidth + 'px';
+                line.style.left = element.offsetLeft + 'px';
+            } else {
+                line.style.width = 0;
+            }
+        }
+
+        if (nav) {
+            var i = 0,
+                line = nav.querySelector('.nav__line'),
+                item = nav.querySelectorAll('.nav__item'),
+                current = nav.querySelector('.is-current'),
+                itemLength = item.length;
+
+            lineStyle(line, current);
+
+            nav.addEventListener('mouseleave', function () {
+                lineStyle(line, current);
+            });
+
+            for (i; i < itemLength; i++) {
+                item[i].addEventListener('mouseover', function () {
+                    lineStyle(line, this);
+                });
+            }
+        }
+
+    }
+
 })(jQuery, window.Modernizr);
